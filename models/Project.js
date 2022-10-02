f3d05e99b0246daeab3070c79bb7f0415eb6b161
Alliegie <100817906+AllieGie@users.noreply.github.com>
2022-10-02 10:38:52 -0400

    added Project schema

---
+++ models/Project.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const ProjectSchema = new mongoose.Schema({
+  projectName: {
+    type: String,
+    required: true,
+  },
+  taskName: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: String,
+    ref: mongoose.Schema.Types.ObjectId,
+    // ref: "User",
+  },
+  userName: {
+    type: String,
+    required: true
+  },
+  notes: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Comment",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    },
+  dueDate: {
+    type: Date,
+    default: Date,
+      },
+});
+
+module.exports = mongoose.model("Project", ProjectSchema);
