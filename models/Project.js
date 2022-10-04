ad9c3898a6e697dd091914f75eeb1d4413aca4d0
Alliegie <100817906+AllieGie@users.noreply.github.com>
2022-10-03 22:25:23 -0400

    updated Project schema variable

--- models/Project.js
+++ models/Project.js
@@ -24,7 +24,7 @@ const ProjectSchema = new mongoose.Schema({
   },
   notes: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "Comment",
+    ref: "Note",
   },
   createdAt: {
     type: Date,
