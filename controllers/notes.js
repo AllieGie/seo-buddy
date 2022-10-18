a42777f53313470d7ea1fef75e25e0b4e97d32e3
Alliegie <100817906+AllieGie@users.noreply.github.com>
2022-10-18 15:02:40 -0400

    updated createNote property

--- controllers/notes.js
+++ controllers/notes.js
@@ -5,7 +5,7 @@ module.exports = {
   createNote: async (req, res) => {
     console.log(req.user.userName)
     try {
-         await Comment.create({ 
+         await Note.create({ 
              note: req.body.note,
              likes: 0,
              user: req.user.id,
@@ -34,14 +34,14 @@ module.exports = {
   },
   deleteNote: async (req, res) => {
     try {
-      // Find Comment by id
+      // Find Note by id
       let Note = await Note.findById({ _id: req.params.id });
-      // Delete Comment from db
+      // Delete Note from db
       await Note.remove({ _id: req.params.id });
       console.log("Deleted Note");
-      res.redirect(`/project/${req.params.id}`);
+      res.redirect(`/Project/${req.params.id}`);
     } catch (err) {
-      res.redirect(`/project/${req.params.id}`);
+      res.redirect(`/Project/${req.params.id}`);
     }
       
       
