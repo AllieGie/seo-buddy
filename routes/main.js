068fa8b2f93d4c142ed8bc1e2c816eae0d6b3885
Alliegie <100817906+AllieGie@users.noreply.github.com>
2022-11-26 14:22:44 -0500

    added assignee routes to main routes

--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
 const projectsController = require("../controllers/projects");
+const dashboardController = require ('../controllers/dashboard')
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
@@ -12,7 +13,10 @@ router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
+router.get("/dashboard", ensureAuth, projectsController.getDashboard)
 router.get("/projects", ensureAuth, projectsController.getProjects);
 router.get("/assignee", ensureAuth, projectsController.getAssignee);
+router.get("/assignees", ensureAuth, projectsController.getAssignee); 
+router.post("/addAssignees", ensureAuth, dashboardController.postAssignee);    
 
 module.exports = router;
