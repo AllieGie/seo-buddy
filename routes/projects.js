78558f6a944bdfeee7d4748752e73805d0cd1575
Alliegie <100817906+AllieGie@users.noreply.github.com>
2022-12-01 14:26:48 -0500

    updated projects routes

--- routes/projects.js
+++ routes/projects.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/multer");
-const projectsController = require("../controllers/projects");
+const projectsController = require("../controllers/projects"); 
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Post Routes - simplified for now
-router.get("/:id", ensureAuth, projectsController.getProject);
+router.get("/:id", ensureAuth, projectsController.getProjects);
+router.get("/:id", ensureAuth, projectsController.getAssignee); 
 
-router.post("/createProject", upload.single("file"), projectsController.createProject);
+router.post("/createProject", ensureAuth, projectsController.createProject);
 
 // router.put("/likeProject/:id", projectsController.likeProject);
 
-router.delete("/deleteProject/:id", projectsController.deletePost);
+router.delete("/deleteProject/:id", ensureAuth, projectsController.deleteProject);
 
 module.exports = router; 
