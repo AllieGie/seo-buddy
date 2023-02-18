d1a667a72a8e9752b80edc86bda85056b1917128
Alliegie <100817906+AllieGie@users.noreply.github.com>
2023-02-18 12:41:35 -0500

    deleted getMembers function, controlling is just posting teamMembers now

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -7,35 +7,35 @@ const TeamMember = require("../models/TeamMembers");
 module.exports = {
   postTeamMembers: async (req, res) => {
     console.log("this is working", req)
-    let teamMemberDetails = req.body //
+    let teamMemberDetails = req.body // req.body = the object {teamMember: 'Keke'}
     console.log('button worked')
-    console.log(teamMemberDetails.teamMember)
+    console.log(teamMemberDetails.teamMember) // teamMemerDetails.teamMember = 'Keke'
     console.log(req.user._id)
     try {
-      const addedteamMembers = await TeamMember.create({
-        name: teamMemberDetails.teamMember,
-        teamLead: req.user._id
+      const addedteamMembers = await TeamMember.create({ //await TeamMember module wwhich is coming from the Model
+        name: teamMemberDetails.teamMember, // this is helping us isolate just the name 
+        teamLead: req.user._id // this is the comes from the user logged in 
       })
-      res.render("teamMembers.ejs", { members: addedteamMembers._id});
-      console.log("team-member added")
+      res.redirect("/teamMembers") 
       
     } catch (err) {
       console.log(err);
     }
-  },
-  getTeamMembers: async (req, res) => {
-    try {
-      // const project = await Project.find().sort({ teamMembers: "desc" }).lean();
-      // we create this object that gets sent to teamembers.ejs so that the info can be accessed
-      const teamMembers = await TeamMember.find({ teamLead: req.user.id })
-      console.log('this is req.user.id', req.user.id)
-      console.log('this went through', teamMembers)
-      res.render("teamMembers.ejs", {  teamMembers: teamMembers, user: req.user });
-      console.log(req.body)
-    } catch (err) { 
-      console.log(err);
-    }
   }
+  // getTeamMembers: async (req, res) => {
+  //   try {
+  //     // const project = await Project.find().sort({ teamMembers: "desc" }).lean();
+  //     // we create this object that gets sent to teamembers.ejs so that the info can be accessed
+  //     const resultsFromTmCollection= await TeamMember.find({ teamLead: req.user.id }) // using the info fromm the req.user to query but not coming from the DB
+  //     console.log('this is req.user.id', req.user.id)
+      
+  //     console.log('this went through', resultsFromTmCollection)
+  //     res.render("teamMembers.ejs", {  teamMembers:resultsFromTmCollection , user: req.user });
+  //     console.log(req.body)
+  //   } catch (err) { 
+  //     console.log(err);
+  //   }
+  // }
 }
     // addTeamMemberss: async (req, res) => {
     //     console.log('clicked')
