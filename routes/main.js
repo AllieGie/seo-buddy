2dc8e189fbc37aa53eed4f2ebacb9d8bc012aecb
Alliegie <100817906+AllieGie@users.noreply.github.com>
2023-02-25 12:01:45 -0500

    added routing for Profile GET request

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
 const projectsController = require("../controllers/projects");
-const dashboardController = require ('../controllers/dashboard')
+const dashboardController = require('../controllers/dashboard')
+const profileController = require("../controllers/profile")
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
@@ -17,6 +18,7 @@ router.get("/dashboard", ensureAuth, projectsController.getDashboard)
 router.get("/projects", ensureAuth, projectsController.getProjects);
 router.post("/projects/createProject", ensureAuth, projectsController.createProject)
 router.get("/teamMembers", ensureAuth, projectsController.getMembers);
+router.get("/profile", ensureAuth, profileController.getProfile)
 // create a new function in Profile controller, 
 router.get("/teamMembers/:id", ensureAuth, projectsController.getMembers); 
 router.post("/teamMembers", ensureAuth, dashboardController.postTeamMembers);    
